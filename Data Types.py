c43d37f1476b1b04bb155cdd7a5b82dcaeb035cb
LeviAndroid18 <rjsumonod@gmail.com>
2018-03-14 10:17:34 +0400

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+# This is an example of data type "lists"
+my_list = ['a','b','c','d']
+print(my_list)
+
+my_list_mod = 3*my_list
+print(my_list_mod)
+
+
+# In[10]:
+
+
+# This data type is called "tuples" - A lists that CANNOT BE CHANGE
+my_tuple = ('a','b','c','d')
+print(my_tuple)
+
+
+# In[11]:
+
+
+my_tuple_mod = 3*my_tuple
+print(my_tuple_mod)
+
+
+# In[17]:
+
+
+# what I hate the most! Data type called dictionary. I guess this is just a fancy term for 'association'. left = key; right = value
+dick_tionary = {1:'a',2:'b',3:'c',4:'d'}
+print(dick_tionary)
+
+
+# In[18]:
+
+
+#isolating some real shit of the dick_tionary
+print(dick_tionary[1])
+
